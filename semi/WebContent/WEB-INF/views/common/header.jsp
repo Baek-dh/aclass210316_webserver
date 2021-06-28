26dca29caf99af5e28975698abaed5055a8e649c
Baek Dong Hyeon <baekdh@iei.or.kr>
2021-06-28 16:47:24 +0900

    게시글 목록 조회(페이징처리)

--- semi/WebContent/WEB-INF/views/common/header.jsp
+++ semi/WebContent/WEB-INF/views/common/header.jsp
@@ -1,4 +1,4 @@
-#<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 
 <%-- 배포된 웹 애플리케이션의 최상위 주소를 간단히 얻어올 수 있도록 
--- semi/src/edu/kh/semi/board/model/dao/SelectBoardDAO.java
+++ semi/src/edu/kh/semi/board/model/dao/SelectBoardDAO.java
@@ -3,11 +3,18 @@ package edu.kh.semi.board.model.dao;
 import static edu.kh.semi.common.JDBCTemplate.*;
 
 import java.io.FileInputStream;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
+import edu.kh.semi.board.model.vo.Board;
+import edu.kh.semi.board.model.vo.Pagination;
 import edu.kh.semi.member.model.dao.MemberDAO;
 
 public class SelectBoardDAO {
@@ -37,6 +44,108 @@ public class SelectBoardDAO {
 			e.printStackTrace();
 		}
 	}
+
+
+	/** 전체 게시글 수 + 게시판 이름 조회 DAO
+	 * @param conn
+	 * @param cp
+	 * @param boardType
+	 * @return map
+	 * @throws Exception
+	 */
+	public Map<String, Object> getListCount(Connection conn, int cp, int boardType)
+				throws Exception{
+	
+		Map<String, Object> map = new  HashMap<String, Object>();
+		
+		String sql = prop.getProperty("getListCount");
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setInt(1, boardType);
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				map.put( "listCount",  rs.getInt(1) );			
+				map.put( "boardName",  rs.getString(2) );	
+			}
+
+		}finally {
+			close(rs);
+			close(pstmt);
+		}
+		
+		return map;
+	}
+
+
+	/** 게시글 목록 조회 DAO
+	 * @param conn
+	 * @param pagination
+	 * @return boardList
+	 * @throws Exception
+	 */
+	public List<Board> selectBoardList(Connection conn, Pagination pagination) throws Exception{
+	
+		List<Board> boardList = new ArrayList<Board>();
+				
+		String sql = prop.getProperty("selectBoardList");
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setInt(1, pagination.getBoardType());
+			
+			// 조회할 범위를 지정할 변수 선언
+			int startRow = (pagination.getCurrentPage() - 1) * pagination.getLimit() + 1;
+			int endRow = startRow + pagination.getLimit() - 1;
+			
+			pstmt.setInt(2, startRow);
+			pstmt.setInt(3, endRow);
+			
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				// 조회된 한 행의 정보를 board에 set
+				Board board = new Board();
+				
+				board.setBoardNo(rs.getInt("BOARD_NO"));
+				board.setCategoryName(rs.getString("CATEGORY_NM"));
+				board.setBoardTitle(rs.getString("BOARD_TITLE"));
+				board.setMemberName(rs.getString("MEMBER_NM"));
+				board.setReadCount(rs.getInt("READ_COUNT"));
+				board.setCreateDate(rs.getTimestamp("CREATE_DT"));
+				
+				// 1) board의 filePath, fileName에 set할 수 있는 List 객체부터 생성
+				List<String> filePath = new ArrayList<String>();
+				List<String> fileName = new ArrayList<String>();
+				
+				// 2) 생성된 리스트에 DB 조회 결과를 추가
+				filePath.add( rs.getString("FILE_PATH") );
+				fileName.add( rs.getString("FILE_NM") );
+				
+				// 3) 리스트를 board에 set
+				board.setFilePath(filePath);
+				board.setFileName(fileName);
+				
+				
+				// set 완료된 board를 boardList에 추가
+				boardList.add(board);
+			}
+			
+		}finally {
+			close(rs);
+			close(pstmt);
+		}
+		
+		return boardList;
+	}
+	
+	
+	
+	
 	
 	
 	
